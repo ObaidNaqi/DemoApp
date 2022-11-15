764d0e92abbce09507c54591e3d066bb13bb69f2
ObaidNaqi <obaidnaqi12@gmail.com>
2022-11-15 20:58:03 +0500

    Second Commit

--- FirstApp/src/in/coursejava/main/MainApp.java
+++ FirstApp/src/in/coursejava/main/MainApp.java
@@ -8,5 +8,10 @@ public class MainApp {
 		if((++x > 2) || (++y > 2))
 		x++;
 		System.out.println(x + " " + y);
+		
+		StringBuffer sb = new StringBuffer("Obaid");
+		int length = sb.append("Naqi").reverse().length();
+		System.out.println(length);
+		
 	} 
 }
